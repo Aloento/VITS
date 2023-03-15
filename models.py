54b91850a9db9791c8778df0253af41500483b7b
Aloento <11802769+Aloento@users.noreply.github.com>
2023-03-15 21:43:18 +0100

    SynthesizerTrn

--- models.py
+++ models.py
@@ -10,8 +10,9 @@ from torch.nn.utils import weight_norm, remove_weight_norm, spectral_norm
 import attentions
 import commons
 import modules
-import monotonic_align
+from analysis import Pitch
 from commons import init_weights, get_padding
+from monotonic_align import maximum_path
 from pqmf import PQMF
 
 
@@ -812,8 +813,9 @@ class CoMBDBlock(torch.nn.Module):
 
 
 class CoMBD(torch.nn.Module):
-  def __init__(self, use_spectral_norm=False):
+  def __init__(self):
     super(CoMBD, self).__init__()
+
     self.pqmf_list = nn.ModuleList([
       PQMF(4, 192, 0.13, 10.0),  # lv2
       PQMF(2, 256, 0.25, 10.0)  # lv1
@@ -935,7 +937,6 @@ class MDC(torch.nn.Module):
       dilations,
       use_spectral_norm=False
   ):
-
     super(MDC, self).__init__()
     norm_f = weight_norm if not use_spectral_norm else spectral_norm
     self.d_convs = nn.ModuleList()
@@ -990,7 +991,6 @@ class SBDBlock(torch.nn.Module):
       dilations,
       use_spectral_norm=False
   ):
-
     super(SBDBlock, self).__init__()
     norm_f = weight_norm if not use_spectral_norm else spectral_norm
     self.convs = nn.ModuleList()
@@ -1038,6 +1038,7 @@ class MDCDConfig:
   def __init__(self):
     self.pqmf_params = [16, 256, 0.03, 10.0]
     self.f_pqmf_params = [64, 256, 0.1, 9.0]
+
     self.filters = [[64, 128, 256, 256, 256], [64, 128, 256, 256, 256],
                     [64, 128, 256, 256, 256], [32, 64, 128, 128, 128]]
 
@@ -1109,6 +1110,7 @@ class SBD(torch.nn.Module):
     y_d_gs = []
     fmap_rs = []
     fmap_gs = []
+
     y_in = self.pqmf.analysis(y)
     y_hat_in = self.pqmf.analysis(y_hat)
     y_in_f = self.f_pqmf.analysis(y)
@@ -1140,7 +1142,7 @@ class SBD(torch.nn.Module):
 class AvocodoDiscriminator(nn.Module):
   def __init__(self, use_spectral_norm=False):
     super(AvocodoDiscriminator, self).__init__()
-    self.combd = CoMBD(use_spectral_norm)
+    self.combd = CoMBD()
     self.sbd = SBD(use_spectral_norm)
 
   def forward(self, y, ys_hat):
@@ -1252,6 +1254,9 @@ class SynthesizerTrn(nn.Module):
       num_chars: int,
       spec_channels: int,
       segment_size: int,
+      midi_start,
+      midi_end,
+      octave_range,
       inter_channels: int,
       hidden_channels: int,
       filter_channels: int,
@@ -1265,6 +1270,10 @@ class SynthesizerTrn(nn.Module):
       upsample_rates: List[int],
       upsample_initial_channel: int,
       upsample_kernel_sizes: List[int],
+      yin_channels,
+      yin_start,
+      yin_scope,
+      yin_shift_range,
       n_speakers=0,
       gin_channels=0,
       use_sdp=True,
@@ -1314,6 +1323,10 @@ class SynthesizerTrn(nn.Module):
     self.gin_channels = gin_channels
     self.use_sdp = use_sdp
 
+    self.yin_channels = yin_channels
+    self.yin_start = yin_start
+    self.yin_scope = yin_scope
+
     self.text_encoder = TextEncoder(
       num_chars,
       inter_channels,
@@ -1326,7 +1339,8 @@ class SynthesizerTrn(nn.Module):
     )
 
     self.waveform_decoder = HifiganGenerator(
-      inter_channels,
+      inter_channels - yin_channels +
+      yin_scope,
       resblock_type,
       resblock_kernel_sizes,
       resblock_dilation_sizes,
@@ -1338,8 +1352,16 @@ class SynthesizerTrn(nn.Module):
 
     self.posterior_encoder = PosteriorEncoder(
       spec_channels,
-      inter_channels,
-      hidden_channels,
+      inter_channels - yin_channels,
+      inter_channels - yin_channels,
+      5, 1, 16,
+      gin_channels=gin_channels
+    )
+
+    self.pitch_encoder = PosteriorEncoder(
+      yin_channels,
+      yin_channels,
+      yin_channels,
       5, 1, 16,
       gin_channels=gin_channels
     )
@@ -1360,10 +1382,44 @@ class SynthesizerTrn(nn.Module):
         hidden_channels, 256, 3, 0.5, gin_channels=gin_channels
       )
 
-    if n_speakers > 1:
-      self.emb_g = nn.Embedding(n_speakers, gin_channels)
+    self.yin_decoder = YingDecoder(
+      yin_scope,
+      5, 1, 4,
+      yin_start,
+      yin_scope,
+      yin_shift_range,
+      gin_channels=gin_channels
+    )
 
-  def forward(self, x, x_lengths, y, y_lengths, sid=None):
+    self.emb_g = nn.Embedding(self.n_speakers, gin_channels)
+    self.pitch = Pitch(
+      midi_start=midi_start,
+      midi_end=midi_end,
+      octave_range=octave_range
+    )
+
+  def crop_scope(self, x: list, scope_shift=0):  # TODO: need to modify for non-scalar shift
+    return [
+      i[:, self.yin_start + scope_shift:self.yin_start + self.yin_scope + scope_shift, :] for i in x
+    ]
+
+  def crop_scope_tensor(
+      self, x: Tensor,  # x: [B,C,T]
+      scope_shift: Tensor  # scope_shift: tensor [B]
+  ):
+    return torch.stack([
+      x[i, self.yin_start + scope_shift[i]:self.yin_start + self.yin_scope + scope_shift[i], :] for i in
+      range(x.shape[0])
+    ], dim=0)
+
+  def yin_dec_infer(self, z_yin, z_mask, sid=None):
+    if self.n_speakers > 0:
+      g = self.emb_g(sid).unsqueeze(-1)  # [b, h, 1]
+    else:
+      g = None
+    return self.yin_decoder.infer(z_yin, z_mask, g)
+
+  def forward(self, x, t, x_lengths, y, y_lengths, ying, ying_lengths, sid=None, scope_shift=0):
     # 对输入序列进行编码
     x, m_p, logs_p, x_mask = self.text_encoder(x, x_lengths)
 
@@ -1373,9 +1429,26 @@ class SynthesizerTrn(nn.Module):
     else:
       g = None
 
-    z, m_q, logs_q, y_mask = self.posterior_encoder(y, y_lengths, g=g)
-    # 通过 Spline Flow 将输出 z 从后验分布转换为先验分布
+    z_spec, m_spec, logs_spec, spec_mask = self.posterior_encoder(y, y_lengths, g=g)
+
+    z_yin, m_yin, logs_yin, yin_mask = self.pitch_encoder(ying, y_lengths, g=g)
+    z_yin_crop, logs_yin_crop, m_yin_crop = self.crop_scope([z_yin, logs_yin, m_yin], scope_shift)
+
+    yin_gt_crop, yin_gt_shifted_crop, yin_dec_crop, z_yin_crop_shifted, scope_shift = self.yin_decoder(
+      z_yin, ying, yin_mask, g)
+
+    z = torch.cat([z_spec, z_yin], dim=1)
+    logs_q = torch.cat([logs_spec, logs_yin], dim=1)
+    m_q = torch.cat([m_spec, m_yin], dim=1)
+    y_mask = spec_mask
+
+    # 通过 Spline Flow 将输出 z_spec 从后验分布转换为先验分布
     z_p = self.flow(z, y_mask, g=g)
+
+    z_dec = torch.cat([z_spec, z_yin_crop], dim=1)
+
+    z_dec_shifted = torch.cat([z_spec.detach(), z_yin_crop_shifted], dim=1)
+    z_dec_ = torch.cat([z_dec, z_dec_shifted], dim=0)
 
     # 计算注意力分布。此部分使用负交叉熵损失来计算注意力分布，以便在生成语音时自动学习对齐模式。
     # 计算了4个不同的贡献，然后将它们相加以获得最终负交叉熵损失。
@@ -1386,16 +1459,16 @@ class SynthesizerTrn(nn.Module):
       s_p_sq_r = torch.exp(-2 * logs_p)
       # [b, 1, t_s]
       neg_cent1 = torch.sum(-0.5 * math.log(2 * math.pi) - logs_p, [1], keepdim=True)
+      # [b, t_t, d] x [b, d, t_s] = [b, t_t, t_s], z_p: [b,d,t]
+      neg_cent2 = torch.einsum('bdt, bds -> bts', -0.5 * (z_p ** 2), s_p_sq_r)
       # [b, t_t, d] x [b, d, t_s] = [b, t_t, t_s]
-      neg_cent2 = torch.matmul(-0.5 * (z_p ** 2).transpose(1, 2), s_p_sq_r)
-      # [b, t_t, d] x [b, d, t_s] = [b, t_t, t_s]
-      neg_cent3 = torch.matmul(z_p.transpose(1, 2), (m_p * s_p_sq_r))
+      neg_cent3 = torch.einsum('bdt, bds -> bts', z_p, (m_p * s_p_sq_r))
       # [b, 1, t_s]
       neg_cent4 = torch.sum(-0.5 * (m_p ** 2) * s_p_sq_r, [1], keepdim=True)
       neg_cent = neg_cent1 + neg_cent2 + neg_cent3 + neg_cent4
 
-      attn_mask = torch.unsqueeze(x_mask, 2) * torch.unsqueeze(y_mask, -1)
-      attn = monotonic_align.maximum_path(neg_cent, attn_mask.squeeze(1)).unsqueeze(1).detach()
+      attn_mask = torch.unsqueeze(x_mask, 2) * torch.unsqueeze(spec_mask, -1)
+      attn = maximum_path(neg_cent, attn_mask.squeeze(1)).unsqueeze(1).detach()
 
     w = attn.sum(2)
     # 根据注意力分布计算权重向量，以调整 x 序列的持续时间。
@@ -1411,18 +1484,50 @@ class SynthesizerTrn(nn.Module):
 
     # expand prior
     # 使用注意力分布对先验分布的均值和方差进行加权，以获得特定时间步的平均均值和方差，从而用于音频生成
-    m_p = torch.matmul(attn.squeeze(1), m_p.transpose(1, 2)).transpose(1, 2)
-    logs_p = torch.matmul(attn.squeeze(1), logs_p.transpose(1, 2)).transpose(1, 2)
+    m_p = torch.einsum('bctn, bdn -> bdt', attn, m_p)
+    logs_p = torch.einsum('bctn, bdn -> bdt', attn, logs_p)
 
     # 随机选择一个小的时间片段 z_slice 和一个对应的 ID 序列 ids_slice，
     # 并将它们传递给波形解码器 waveform_decoder，用于生成音频
-    z_slice, ids_slice = commons.rand_slice_segments(z, y_lengths, self.segment_size)
-    o = self.waveform_decoder(z_slice, g=g)
+    z_slice, ids_slice = commons.rand_slice_segments_for_cat(
+      z_dec_, torch.cat([y_lengths, y_lengths], dim=0), self.segment_size
+    )
+
+    o_ = self.dec.hier_forward(z_slice, g=torch.cat([g, g], dim=0))
+
+    o = [torch.chunk(o_hier, 2, dim=0)[0] for o_hier in o_]
     # 返回生成的音频 o，持续时间的损失 l_length，注意力分布 attn，
     # 随机选择的时间片段的 ID 序列 ids_slice，以及用于将来计算注意力的 x_mask 和 `y
-    return o, l_length, attn, ids_slice, x_mask, y_mask, (z, z_p, m_p, logs_p, m_q, logs_q)
+    o_pad = F.pad(
+      o_[-1],
+      (768, 768 + (-o_[-1].shape[-1]) % 256 + 256 * (o_[-1].shape[-1] % 256 == 0)),
+      mode='constant'
+    ).squeeze(1)
 
-  def infer(self, x, x_lengths, sid=None, noise_scale=1, length_scale=1, noise_scale_w=1., max_len=None):
+    yin_hat = self.pitch.yingram(o_pad)
+    yin_hat_crop = self.crop_scope([yin_hat])[0]
+    yin_hat_shifted = self.crop_scope_tensor(
+      torch.chunk(yin_hat, 2, dim=0)[0], scope_shift
+    )
+
+    return o, l_length, attn, ids_slice, x_mask, spec_mask, o_, \
+      (z, z_p, m_p, logs_p, m_q, logs_q), \
+      z_dec_, \
+      (z_spec, m_spec, logs_spec, spec_mask, z_yin, m_yin, logs_yin, yin_mask), \
+      (yin_gt_crop, yin_gt_shifted_crop, yin_dec_crop, yin_hat_crop, scope_shift, yin_hat_shifted)
+
+  def infer(
+      self,
+      x,
+      t,
+      x_lengths,
+      sid=None,
+      noise_scale=1,
+      length_scale=1,
+      noise_scale_w=1.,
+      max_len=None,
+      scope_shift=0  # TODO: need to fix vector scope shift needed
+  ):
     x, m_p, logs_p, x_mask = self.text_encoder(x, x_lengths)
 
     if self.n_speakers > 0:
@@ -1443,15 +1548,148 @@ class SynthesizerTrn(nn.Module):
     attn = commons.generate_path(w_ceil, attn_mask)
 
     # [b, t', t], [b, t, d] -> [b, d, t']
-    m_p = torch.matmul(attn.squeeze(1), m_p.transpose(1, 2)).transpose(1, 2)
+    m_p = torch.einsum('bctn, bdn -> bdt', attn, m_p)
     # [b, t', t], [b, t, d] -> [b, d, t']
-    logs_p = torch.matmul(attn.squeeze(1), logs_p.transpose(1, 2)).transpose(1, 2)
+    logs_p = torch.einsum('bctn, bdn -> bdt', attn, logs_p)
 
     z_p = m_p + torch.randn_like(m_p) * torch.exp(logs_p) * noise_scale
     z = self.flow(z_p, y_mask, g=g, reverse=True)
-    o = self.waveform_decoder((z * y_mask)[:, :, :max_len], g=g)
+    z_spec, z_yin = torch.split(
+      z,
+      self.inter_channels - self.yin_channels,
+      dim=1
+    )
+    z_yin_crop = self.crop_scope([z_yin], scope_shift)[0]
+    z_crop = torch.cat([z_spec, z_yin_crop], dim=1)
+    o = self.dec((z_crop * y_mask)[:, :, :max_len], g=g)
 
-    return o, attn, y_mask, (z, z_p, m_p, logs_p)
+    return o, attn, y_mask, (z_crop, z, z_p, m_p, logs_p)
+
+  def infer_pre_decoder(
+      self,
+      x,
+      t,
+      x_lengths,
+      sid=None,
+      noise_scale=1.,
+      length_scale=1.,
+      noise_scale_w=1.,
+      max_len=None,
+      scope_shift=0
+  ):
+    x, m_p, logs_p, x_mask = self.text_encoder(x, t, x_lengths)
+
+    if self.n_speakers > 0:
+      g = self.emb_g(sid).unsqueeze(-1)  # [b, h, 1]
+    else:
+      g = None
+
+    if self.use_sdp:
+      logw = self.duration_predictor(
+        x,
+        x_mask,
+        g=g,
+        reverse=True,
+        noise_scale=noise_scale_w
+      )
+    else:
+      logw = self.duration_predictor(x, x_mask, g=g)
+
+    w = torch.exp(logw) * x_mask * length_scale
+    w_ceil = torch.ceil(w)
+    y_lengths = torch.clamp_min(torch.sum(w_ceil, [1, 2]), 1).long()
+    y_mask = torch.unsqueeze(commons.sequence_mask(y_lengths, None), 1).to(x_mask.dtype)
+    attn_mask = torch.unsqueeze(x_mask, 2) * torch.unsqueeze(y_mask, -1)
+    attn = commons.generate_path(w_ceil, attn_mask)
+
+    m_p = torch.einsum('bctn, bdn -> bdt', attn, m_p)
+    logs_p = torch.einsum('bctn, bdn -> bdt', attn, logs_p)
+
+    z_p = m_p + torch.randn_like(m_p) * torch.exp(logs_p) * noise_scale
+    z = self.flow(z_p, y_mask, g=g, reverse=True)
+    z_spec, z_yin = torch.split(
+      z,
+      self.inter_channels - self.yin_channels,
+      dim=1
+    )
+    z_yin_crop = self.crop_scope([z_yin], scope_shift)[0]
+    z_crop = torch.cat([z_spec, z_yin_crop], dim=1)
+    decoder_inputs = z_crop * y_mask
+
+    return decoder_inputs, attn, y_mask, (z_crop, z, z_p, m_p, logs_p)
+
+  def infer_pre_lr(
+      self,
+      x,
+      t,
+      x_lengths,
+      sid=None,
+      length_scale=1,
+      noise_scale_w=1.,
+  ):
+    x, m_p, logs_p, x_mask = self.text_encoder(x, t, x_lengths)
+
+    if self.n_speakers > 0:
+      g = self.emb_g(sid).unsqueeze(-1)  # [b, h, 1]
+    else:
+      g = None
+
+    if self.use_sdp:
+      logw = self.duration_predictor(
+        x,
+        x_mask,
+        g=g,
+        reverse=True,
+        noise_scale=noise_scale_w
+      )
+    else:
+      logw = self.duration_predictor(x, x_mask, g=g)
+
+    w = torch.exp(logw) * x_mask * length_scale
+    w_ceil = torch.ceil(w)
+
+    return w_ceil, x, m_p, logs_p, x_mask, g
+
+  def infer_lr(self, w_ceil, x, m_p, logs_p, x_mask):
+    y_lengths = torch.clamp_min(torch.sum(w_ceil, [1, 2]), 1).long()
+    y_mask = torch.unsqueeze(commons.sequence_mask(y_lengths, None), 1).to(x_mask.dtype)
+    attn_mask = torch.unsqueeze(x_mask, 2) * torch.unsqueeze(y_mask, -1)
+    attn = commons.generate_path(w_ceil, attn_mask)
+
+    m_p = torch.einsum('bctn, bdn -> bdt', attn, m_p)
+    logs_p = torch.einsum('bctn, bdn -> bdt', attn, logs_p)
+
+    return m_p, logs_p, y_mask
+
+  def infer_post_lr_pre_decoder(
+      self,
+      m_p,
+      logs_p,
+      g,
+      y_mask,
+      noise_scale=1,
+      scope_shift=0
+  ):
+    z_p = m_p + torch.randn_like(m_p) * torch.exp(logs_p) * noise_scale
+    z = self.flow(z_p, y_mask, g=g, reverse=True)
+    z_spec, z_yin = torch.split(
+      z,
+      self.inter_channels - self.yin_channels,
+      dim=1
+    )
+
+    z_yin_crop = self.crop_scope([z_yin], scope_shift)[0]
+    z_crop = torch.cat([z_spec, z_yin_crop], dim=1)
+    decoder_inputs = z_crop * y_mask
+
+    return decoder_inputs, y_mask, (z_crop, z, z_p, m_p, logs_p)
+
+  def infer_decode_chunk(self, decoder_inputs, sid=None):
+    if self.n_speakers > 0:
+      g = self.emb_g(sid).unsqueeze(-1)  # [b, h, 1]
+    else:
+      g = None
+    return self.dec(decoder_inputs, g=g)
 
   def voice_conversion(self, y, y_lengths, sid_src, sid_tgt):
     assert self.n_speakers > 0, "n_speakers have to be larger than 0."
